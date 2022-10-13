5174aa420cc8c4508fcee12468407c92afa9c419
github <github1017053+mikiookubo@users.noreply.github.com>
2022-10-13 16:41:28 +0900

    first commit

---
+++ js_learning/js/operator_05.js
@@ -0,0 +1,6 @@
+let multiplication = 8*3;
+let multiplication2 = multiplication++;
+let multiplication3 = multiplication2--;
+console.log(8*3);
+console.log(multiplication);
+console.log(multiplication2);
