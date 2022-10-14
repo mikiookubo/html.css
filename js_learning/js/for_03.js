e497db0fbbb22cca4b007940d87f2ebcf932c171
github <github1017053+mikiookubo@users.noreply.github.com>
2022-10-14 20:34:20 +0900

    sencomd commit

--- js_learning/js/for_03.js
+++ js_learning/js/for_03.js
@@ -2,7 +2,7 @@
 const eighteenYearsold = () =>{
     const participant =[{name:'taro',age:20},{name:'hanako' ,age:15} ,{name:'john',age:25}];
 
-    let car = participant.map(i=>{
+    let old = participant.map(i=>{
         if (i.age >=18){
             console.log(i.name+"さんは18歳以上のため参加可能です");
         }else{
