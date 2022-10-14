442b257df6b1fc230fcbc94e8a86c9ea4b0ebcf0
github <github1017053+mikiookubo@users.noreply.github.com>
2022-10-14 23:24:12 +0900

    second commit

--- js_learning/js/for_03.js
+++ js_learning/js/for_03.js
@@ -1,12 +1,13 @@
 
 const eighteenYearsold = () =>{
     const participant =[{name:'taro',age:20},{name:'hanako' ,age:15} ,{name:'john',age:25}];
-
-    let old = participant.map(i=>{
-        if (i.age >=18){
-            console.log(i.name+"さんは18歳以上のため参加可能です");
+    for(let p of participant){
+        if(p.age >= 18){
+            console.log(p.name+"さんは18歳以上のため参加可能です");
         }else{
-            console.log(i.name+"さんは18歳未満のため参加できません");
+            console.log(p.name+"さんは18歳未満のため参加できません");
         }
-    })};
+    }};
+
+   
     console.log(eighteenYearsold());
